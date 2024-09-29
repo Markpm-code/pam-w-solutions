68f8b0ecf4255bcc6a4798f0d8ca09542e7f2242
Marlon <you@example.com>
2024-09-29 09:59:47 +0100

    add navigation layout, styling and routing

--- pam-websolutions/src/App.tsx
+++ pam-websolutions/src/App.tsx
@@ -1,11 +1,39 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Services from "./pages/Services";
+import NotFound from "./pages/NotFound";
+import Footer from "./components/Footer";
+
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<NavBar />}>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="services" element={<Services />} />
+
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
 
 function App() {
 
   return (
     <>
-      
+         <RouterProvider router={router} />
+         <Footer />
     </>
   )
 }
--- pam-websolutions/src/components/NavBar.tsx
+++ pam-websolutions/src/components/NavBar.tsx
@@ -1,8 +1,38 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link, Outlet, NavLink } from "react-router-dom";
+import "./NavBar.css";
+import Logo from "../assets/images/pam-logo1.png";
+
 
 const NavBar = () => {
   return (
     <div>
-      
+      <Navbar expand="lg" className="bg-secondary">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            <img className="logo" src={Logo} alt="" />
+            Pam Web Soultions
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-content-end"
+          >
+            <Nav className="navigation-links">
+              <NavLink to="/">Home</NavLink>
+              <NavLink to="about">About</NavLink>
+              <NavLink to="contact">Contact</NavLink>
+              <NavLink to="services">Services</NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <main>
+        <Outlet />
+      </main>
     </div>
   )
 }
